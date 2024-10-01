5d73f2dc4f414948b47b2ccf62e48bbadba6a37b
csGiovanni <116302921+csGiovanni@users.noreply.github.com>
2024-10-01 10:05:41 -0500

    Add Basic input

--- Circle.lua
+++ Circle.lua
@@ -1,9 +1,16 @@
 local time = 0
+local positions = {{150, 150}}
 function DrawCircle()
     love.graphics.print("MY TEST", 400, 200)
-    love.graphics.circle("fill", 150 + math.sin(time) * 50, 150, 40, 20)
+    for i, v in pairs(positions) do
+        love.graphics.circle("fill", v[1] + math.sin(time) * 50, v[2], 40, 20)
+    end
+    
 end
 
 function UpdateCircle(dt)
     time = time + math.pi * dt
+end
+function AddCircle(x, y)
+    table.insert(positions, {x, y})
 end
--- main.lua
+++ main.lua
@@ -3,9 +3,17 @@ require("Circle")
 function love.load()
     
 end
-
+local click_debounce = false
 function love.update(dt)
     UpdateCircle(dt)
+    if (love.mouse.isDown(1)) then
+        if (click_debounce == false) then
+            click_debounce = true
+            AddCircle(love.mouse.getX(), love.mouse.getY())
+        end
+    else
+        click_debounce = false
+    end
 end
 
 function love.draw()
